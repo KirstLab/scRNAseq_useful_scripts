19f69a48aa5f4d31dd00946ba080b65d8d41a3ce
Wendell Pereira <wendelljpereira@gmail.com>
2023-07-14 10:06:51 -0400

    Adds monocle3_to_seurat folder.

---
+++ monocle3_to_seurat/README.md
@@ -0,0 +1,17 @@
+# Converting data from Monocle3 object to Seurat object
+
+Many tools expect data in Seurat format. This script converts data from a Monocle3 object to a Seurat object. Note that it expects the Monocle3 object to be saved in a RDS file and the dataset to be fully processed (i.e. normalized, clustered, etc.).
+
+The following information is transferred from the Monocle3 object to the Seurat object:
+
+* Raw counts
+* Normalized expression matrix
+* Dimensionality reduction (PCA and UMAP)
+* Clusters
+* Other metadata (e.g. cell type, sample, etc.)
+
+To run the script, first set the name of the Monocle3 object file and the name of the Seurat object file within the script. Then, run the script in a terminal (or execute line by line in RStudio).
+
+```bash
+Rscript monocle3_to_seurat.R
+```
---
+++ monocle3_to_seurat/monocle3_to_seurat.R
@@ -0,0 +1,79 @@
+# Set a seed increase reproducibility
+set.seed(1407)
+
+# Load required packages
+require(Seurat)
+require(monocle3)
+require(vroom)
+require(tidyverse)
+
+################################
+## Parameters set by the user ##
+
+MONOCLE3_OBJ <- "your_clustered_monocle3_obj.rds" # name of the input monocle3 object
+SEURAT_OBJ <- "your_seurat_obj.rds" # name of the output seurat object
+
+################################
+
+# Read the RDS file
+my.cds <- readRDS(MONOCLE3_OBJ)
+
+# Add the cluster information to the monocle3 object
+colData(my.cds)$cell_type <- monocle3::clusters(my.cds)
+
+# Plot the monocle3 dataset, to confirm all is working
+p1 <- plot_cells(my.cds,
+           label_cell_groups = T,
+           graph_label_size = 1.5,
+           cell_size = 1,
+           group_label_size = 7)
+
+## Save the plot
+ggsave("monocle3_plot.png", p1, width = 10, height = 10, units = "in")
+
+# transfer the counts from the monocle object
+rna_counts <- monocle3::exprs(my.cds)
+
+## Create a new Seurat Object using the raw counts
+adata <- CreateSeuratObject(counts = rna_counts,
+                            assay = "RNA",
+                            project = "scMedicago" # change this to your sample
+                            )
+
+# Merge the data normalized counts to the object
+
+## Gather the normalized counts
+rna_data <- monocle3::normalized_counts(my.cds)
+
+## Merge the normalized counts to the object
+rna_counts_assay <- CreateAssayObject(counts = rna_counts)
+rna_counts_assay@key <- "RNA_"
+adata@assays$RNA <- rna_counts_assay
+adata@assays$RNA@data <- rna_data
+
+# Add the cell embeddings for UMAP
+mat_emb <- reducedDims(my.cds)$UMAP
+UMA_assay <- CreateDimReducObject(embeddings = mat_emb, key = 'UMAP_' )
+UMA_assay@key <- "UMAP_"
+adata@reductions$UMAP <- UMA_assay
+
+# Add the cell embeddings for PCA
+mat_emb <- reducedDims(my.cds)$PCA
+pca_assay <- CreateDimReducObject(embeddings = mat_emb, key = 'pca_' )
+pca_assay@key <- "pca_"
+adata@reductions$pca <- pca_assay
+
+# Add clustering and other meta information to the object
+colData( my.cds )$seurat_clusters <- colData( my.cds )$cell_type
+adata <- AddMetaData(adata, as.data.frame( colData( my.cds ) ) )
+
+# Creates a UMAP plot to test if conversion worked
+p2 <- Seurat::DimPlot(adata, reduction = 'UMAP',
+                group.by = "seurat_clusters",
+                pt.size = 1 )
+
+## Save the plot
+ggsave("seurat_plot.png", p2, width = 10, height = 10, units = "in")
+
+# Save the seurat formated dataset as a RDS file
+saveRDS(adata, SEURAT_OBJ)
